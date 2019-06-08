7c7477cb1496cba5febba863b0eeff21fcb148a2
Hoang Le <hoang.le4@pcc.edu>
2019-06-08 16:20:25 -0700

    commit starting point

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const router = require('express').Router();
+const apiRoutes = require('./api');
+const dbRoutes = require('./db');
+
+// API Routes
+router.use('/api', apiRoutes);
+
+// DB Routes
+router.use('/db', dbRoutes);
+
+// If no API routes are hit, send the React app
+router.use(function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
+//Backend API
+app.use(routes);
+
+// Connect to the Mongo DB
+const MONGODB_URI = require('./config/mongoDB');
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser: true
+});
+
+app.listen(PORT, function() {
+	console.log(`API Server now listening on PORT ${PORT}!`);
+});
