6adcd9420e8544c1d2c10bd8296837a28c07c196
hoangle4 <hoang.le4@pcc.edu>
2019-06-10 01:15:43 -0700

    commit initial map

--- client/src/components/Home/Map.js
+++ client/src/components/Home/Map.js
@@ -1,20 +1,41 @@
 import React, { Component, Fragment } from "react";
 import MapBox from "mapbox-gl/dist/mapbox-gl.js";
 class Map extends Component {
-  componentDidMount = () => {
-    MapBox.accessToken = process.env.REACT_APP_MAP_BOX_KEY;
-    this.map = new MapBox.Map({
-      container: this.mapContainer,
-      style: "mapbox://styles/mapbox/streets-v11"
-    });
+  state = {
+    geolocation: {
+      currentLongitude: "",
+      currentLatitude: ""
+    }
+  };
+  componentDidMount = async () => {
+    navigator.geolocation.getCurrentPosition(
+      async position => {
+        this.setState({
+          currentLongitude: position.coords.longitude,
+          currentLatitude: position.coords.latitude
+        });
+      },
+      error => alert(error.message),
+      {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000
+      }
+    );
   };
 
   componentWillUnmount = () => {
     this.map.remove();
   };
 
-  componentDidUpdate = e => {
-    console.log(e);
+  componentDidUpdate = () => {
+    MapBox.accessToken = process.env.REACT_APP_MAP_BOX_KEY;
+    this.map = new MapBox.Map({
+      center: [this.state.currentLongitude, this.state.currentLatitude],
+      container: this.mapContainer,
+      zoom: 10,
+      style: "mapbox://styles/mapbox/streets-v11"
+    });
   };
   render() {
     const style = {
