41d4a43913c4e27f61b1f434edcbf4fc4a3461a0
Hoang Le <hoang.le4@pcc.edu>
2019-06-12 10:08:06 -0700

    commit toggle navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // import './App.css';
 
-import Home from "./Home";
-import NavTabs from "./Components/NavTabs";
-import Dashboard from "./Dashboard";
-import SignUp from "./Signup";
-import Profile from "./Profile";
-import Gem from "./Gem";
+import Home from './Home';
+import NavTabs from './Components/NavTabs';
+import Dashboard from './Dashboard';
+import SignUp from './Signup';
+import Profile from './Profile';
+import Gem from './Gem';
 import Toolbar from './Components/Toolbar';
 
-
-
-
 class App extends Component {
-  render() {
-    return (
-      <Toolbar />
-      // <Router>
-      //   <div className="Nav-container">
-      //     <NavTabs />
-      //     <Route exact path="/" component={Home} />
-      //     <Route exact path="/signup" component={SignUp} />
-      //     <Route exact path="/gem" component={Gem} />
-      //     <Route exact path="/dashboard" component={Dashboard} />
-      //     <Route exact path="/userprofile" component={Profile} />
-      //   </div>
-      // </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<Toolbar />
+				<div className="Nav-container">
+					{/* <Route exact path="/" component={Home} /> */}
+					<Route exact path="/signup" component={SignUp} />
+					<Route exact path="/gem" component={Gem} />
+					<Route exact path="/dashboard" component={Dashboard} />
+					<Route exact path="/userprofile" component={Profile} />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
--- client/src/Components/Toolbar/index.js
+++ client/src/Components/Toolbar/index.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
-
+import NavTab from '../NavTabs';
 import './Toolbar.css';
 
 class Toolbar extends Component {
-  render() {
-    return (
-      <header className="Toolbar">
-        <nav className="Toolbar-navigation">
-          <h1 className="Toolbar-logo">
-            <a className="Toolbar-logo" href="/">Hidden Gems</a>
-          </h1>
-          <div className="Toolbar-hamburger">
-            <i class="fas fa-bars"></i>
-          </div>
-        </nav>
-      </header>
-    );
-  }
+	state = {
+		isNav: false
+	};
+
+	handleClick = () => {
+		this.setState({
+			isNav: !this.state.isNav
+		});
+	};
+	render() {
+		return (
+			<header className="Toolbar">
+				<nav className="Toolbar-navigation">
+					<h1 className="Toolbar-logo">
+						<a className="Toolbar-logo" href="/">
+							Hidden Gems
+						</a>
+					</h1>
+					{this.state.isNav ? <NavTab /> : null}
+					<div className="Toolbar-hamburger">
+						<a href="#" className="fas fa-bars" onClick={this.handleClick} />
+					</div>
+				</nav>
+			</header>
+		);
+	}
 }
 
 export default Toolbar;
