ed9b5ad6becf2a0b80704b2208a626e235255c64
Ryan Grunest <ryangrunest45@gmail.com>
2019-06-13 09:47:21 -0700

    signup form styling

--- client/src/Signup/index.js
+++ client/src/Signup/index.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import './Signup.css';
 
 class Signup extends Component {
 
   render() {
     return (
-      <div>HelloWorld</div>
-    )
+      <div className="Signup">
+        <div className="Signup-container">
+          <form className="Signup-form">
+            <p className="Signup-input-label">First Name</p>
+            <input className="Signup-form-field" type="text" name="first-name"></input>
+            <p className="Signup-input-label">Last Name</p>
+            <input className="Signup-form-field" type="text" name="last-name"></input>
+            <p className="Signup-input-label">Email</p>
+            <input className="Signup-form-field" type="text" name="email"></input>
+            <p className="Signup-input-label">Password</p>
+            <input className="Signup-form-field" type="text" name="password"></input>
+            <p className="Signup-input-label">Verify Password</p>
+            <input className="Signup-form-field" type="text" name="verify-password"></input>
+            <div className="Signup-submit-btn-container">
+              <button className="Signup-submit-btn" type='submit'>Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
