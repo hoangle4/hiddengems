54fe391cb1d9fccb3bb029bbe7b0d54359cb6a85
Hoang Le <hoang.le4@pcc.edu>
2019-06-13 19:52:58 -0700

    commit sidedraw

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 // import './App.css';
 
 import Home from './Home';
-import NavTabs from './Components/NavTabs';
 import Dashboard from './Dashboard';
 import SignUp from './Signup';
 import Login from './Login';
 import Profile from './Profile';
 import Gem from './Gem';
-import Toolbar from './Components/Toolbar';
-import SideDrawer from './Components/SideDrawer/SideDrawer';
 
 class App extends Component {
-	state = {
-		sideDrawerOpen: false,
-	};
-
-	drawerToggleClickHandler = () => {
-		this.setState((prevState) => {
-			return {sideDrawerOpen: !prevState.sideDrawerOpen}
-		})
-	};
-
 	render() {
-		let sideDrawer;
-		if (this.state.sideDrawerOpen) {
-			sideDrawer = <SideDrawer />;
-		}
-
 		return (
 			<Router>
-				<Toolbar drawerClick={this.drawerToggleClickHandler} />
-				{sideDrawer}
 				<div className="Nav-container">
 					<Route exact path="/" component={Home} />
 					<Route exact path="/signup" component={SignUp} />
--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -1,12 +1,30 @@
-import React, { Component } from "react";
-import Map from "./Map";
+import React, { Component } from 'react';
+import Map from './Map';
+import Toolbar from '../components/Toolbar';
+import SideDrawer from '../components/SideDrawer/SideDrawer';
 class index extends Component {
-  render() {
-    return (
-      <div className="Map-container">
-        <Map />
-      </div>
-    );
-  }
+	state = {
+		sideDrawerOpen: false
+	};
+
+	drawerToggleClickHandler = () => {
+		this.setState((prevState) => {
+			return { sideDrawerOpen: !prevState.sideDrawerOpen };
+		});
+	};
+	render() {
+		let sideDrawer;
+		if (this.state.sideDrawerOpen) {
+			sideDrawer = <SideDrawer />;
+		}
+
+		return (
+			<div className="Map-container">
+				<Toolbar drawerClick={this.drawerToggleClickHandler} />
+				{sideDrawer}
+				<Map />
+			</div>
+		);
+	}
 }
 export default index;
