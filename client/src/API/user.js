4b0aafdeb7118f0ead779ae74454303e007b1584
Ryan Grunest <ryangrunest45@gmail.com>
2019-06-13 22:53:45 -0700

    created user API doc

---
+++ client/src/API/user.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export default {
+  // store initial user
+  saveUser: user => {
+    axios.post('/user', {
+      email: user.email,
+      password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName
+    })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  },
+  // get user
+  getUser: user => {
+    axios.get(`/user?ID=${user.username}&password=${user.password}`)
+    .then(response => {
+      console.log(response);
+    })
+  },
+  // update profile picture
+  updateProfilePicture: user => {
+    axios.post('/update/profile-picture', {
+      url: user.url,
+      // might want to user something else to verify user.. not sure yet
+      user: user.user
+    })
+  }
+  // update bg photo
+  // update password
+}
