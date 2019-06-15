53f746dd4d6d6568981a0482bb6486e41f7a65fd
Hoang Le <hoang.le4@pcc.edu>
2019-06-15 13:58:21 -0700

    commit created place

--- client/src/Components/MapForms/index.js
+++ client/src/Components/MapForms/index.js
@@ -1,87 +1,79 @@
-import React, { Component } from "react";
-import FromGroup from "./FormGroup";
-import db from "../../API/db";
-import firebaseStorage from "../Firebase";
-import "./mapforms.css";
+import React, { Component } from 'react';
+import FromGroup from './FormGroup';
+import db from '../../API/db';
+import firebaseStorage from '../Firebase';
+import './mapforms.css';
 class index extends Component {
-  state = {
-    placeName: "",
-    photos: "",
-    category: "",
-    description: "",
-    progress: "",
-    isUploaded: false
-  };
+	state = {
+		placeName: '',
+		photos: '',
+		category: '',
+		description: '',
+		progress: '',
+		coordinates: '',
+		isUploaded: false
+	};
 
-  componentDidMount = () => {
-    console.log(firebaseStorage);
-  };
+	handleOnChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
-  handleOnChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
+	handleFileChange = (e) => {
+		const { files } = e.target;
+		const { name, type, lastModified, size } = files[0];
+		//Boot out if no files or file bigger than 5MB
+		if (!files || size > 5000000) return;
 
-  handleFileChange = e => {
-    const { files } = e.target;
-    const { name, type, lastModified, size } = files[0];
-    //Boot out if no files or file bigger than 5MB
-    if (!files || size > 5000000) return;
+		const storageRef = firebaseStorage.ref('placePhotos/' + name);
+		const upLoadFile = storageRef.put(files[0], { type });
 
-    const storageRef = firebaseStorage.ref("placePhotos/" + name);
-    const upLoadFile = storageRef.put(files[0], { type });
+		upLoadFile.on(
+			'state_changed',
+			async (results) => {
+				let progress = (await (results.bytesTransferred / results.totalBytes)) * 100;
+				console.log('Upload is ' + progress + '% done');
+				this.setState({ progress: progress });
+			},
+			(err) => console.log(err),
+			() => {
+				// Upload completed successfully, now we can get the download URL
+				upLoadFile.snapshot.ref.getDownloadURL().then((downloadURL) => {
+					this.setState({
+						photos: downloadURL,
+						isUploaded: true
+					});
+				});
+			}
+		);
+	};
 
-    upLoadFile.on(
-      "state_changed",
-      async results => {
-        let progress =
-          (await (results.bytesTransferred / results.totalBytes)) * 100;
-        console.log("Upload is " + progress + "% done");
-        this.setState({ progress: progress });
-      },
-      err => console.log(err),
-      () => {
-        // Upload completed successfully, now we can get the download URL
-        upLoadFile.snapshot.ref.getDownloadURL().then(downloadURL => {
-          this.setState({
-            photos: downloadURL,
-            isUploaded: true
-          });
-        });
-      }
-    );
-  };
+	handleOnClick = async (e) => {
+		await this.setState({ coordinates: this.props.coordinates });
+		const results = await db.createPlace(this.state);
+	};
 
-  handleOnClick = async e => {
-    const results = await db.createPlace(this.state);
-    console.log(results);
-  };
-
-  render() {
-    const { isPinDropped } = this.props;
-    return (
-      <div
-        className="form-group"
-        style={
-          isPinDropped
-            ? { height: "80vh", display: "block" }
-            : { display: "none" }
-        }
-      >
-        <FromGroup
-          value={this.state}
-          progress={this.state.progress}
-          isUploaded={this.state.isUploaded}
-          photos={this.state.photos}
-          handleOnChange={this.handleOnChange}
-          handleOnClick={this.handleOnClick}
-          handleFileChange={this.handleFileChange}
-        />
-      </div>
-    );
-  }
+	render() {
+		const { isPinDropped } = this.props;
+		return (
+			<div
+				className="form-group"
+				style={isPinDropped ? { height: '80vh', display: 'block' } : { display: 'none' }}
+			>
+				<FromGroup
+					value={this.state}
+					progress={this.state.progress}
+					isUploaded={this.state.isUploaded}
+					photos={this.state.photos}
+					handleOnChange={this.handleOnChange}
+					handleOnClick={this.handleOnClick}
+					handleFileChange={this.handleFileChange}
+				/>
+			</div>
+		);
+	}
 }
 
 export default index;
