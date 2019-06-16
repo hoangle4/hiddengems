3cf012b22ba9fb60d2a33ed9e3f19d6c9011e70a
hoangle4 <hoang.le4@pcc.edu>
2019-06-15 23:23:12 -0700

    commit some changes

--- client/src/Gem/index.js
+++ client/src/Gem/index.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 /* import API from "../..; */
 import Banner from "./parts/Banner";
 import Story from "./parts/Story";
-import SideDrawer from "../Components/SideDrawer/SideDrawer";
+import SideDrawer from "../components/SideDrawer/SideDrawer";
 
-import Toolbar from "../Components/Toolbar";
+import Toolbar from "../components/Toolbar";
 
 import gemPic from "../Gem/images/westmorelandpark.jpg";
 import "./style.css";
@@ -17,9 +17,9 @@ class Gem extends Component {
 
     //probably need to adjust these when we have data:
     image: gemPic,
-    title: "Westmoreland Park",
+    title: "Westmoreland Park"
   };
-  
+
   drawerToggleClickHandler = () => {
     this.setState({ sideDrawerOpen: !this.state.sideDrawerOpen });
   };
@@ -28,12 +28,12 @@ class Gem extends Component {
     return (
       <div>
         <Toolbar drawerClick={this.drawerToggleClickHandler} />
-        {
-          this.state.sideDrawerOpen ? <SideDrawer isLoggedIn={this.state.isLoggedIn}/> : null
-        }
+        {this.state.sideDrawerOpen ? (
+          <SideDrawer isLoggedIn={this.state.isLoggedIn} />
+        ) : null}
         <Banner image={this.state.image} />
-        <div className = "container">
-          <Story title={this.state.title}/>
+        <div className="container">
+          <Story title={this.state.title} />
         </div>
       </div>
     );
--- client/src/Home/index.js
+++ client/src/Home/index.js
@@ -1,33 +1,32 @@
-import React, { Component } from 'react';
-import Map from './Map';
-import Toolbar from '../Components/Toolbar';
-import SideDrawer from "../Components/SideDrawer/SideDrawer";
+import React, { Component } from "react";
+import Map from "./Map";
+import Toolbar from "../components/Toolbar";
+import SideDrawer from "../components/SideDrawer/SideDrawer";
 
 class index extends Component {
-	state = {
-		sideDrawerOpen: false,
-		isLoggedIn: false,
-	};
+  state = {
+    sideDrawerOpen: false,
+    isLoggedIn: false
+  };
 
-	drawerToggleClickHandler = () => {
-		this.setState({ sideDrawerOpen: !this.state.sideDrawerOpen });
-	  };
+  drawerToggleClickHandler = () => {
+    this.setState({ sideDrawerOpen: !this.state.sideDrawerOpen });
+  };
 
-	render() {
-		return (
-			<div>
-				<div>
-					<Toolbar drawerClick={this.drawerToggleClickHandler} />
-					{
-					this.state.sideDrawerOpen ? <SideDrawer isLoggedIn={this.state.isLoggedIn}/> : null
-					}
-				</div>
-				<div className="Map-container">
-					<Map />
-				</div>
-			</div>
-			
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div>
+          <Toolbar drawerClick={this.drawerToggleClickHandler} />
+          {this.state.sideDrawerOpen ? (
+            <SideDrawer isLoggedIn={this.state.isLoggedIn} />
+          ) : null}
+        </div>
+        <div className="Map-container">
+          <Map />
+        </div>
+      </div>
+    );
+  }
 }
 export default index;
