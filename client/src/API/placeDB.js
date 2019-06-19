c81c5cc793e676fb55b5762dea506d42d93d5e19
Hoang Le <hoang.le4@pcc.edu>
2019-06-18 20:38:50 -0700

    commit API Find One Place

--- client/src/API/placeDB.js
+++ client/src/API/placeDB.js
@@ -1,14 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
 const db = {
-  createPlace: place => {
-    return axios.post(`/db/createPlace`, {
-      place
-    });
-  },
-  findAllPlace: () => {
-    return axios.get("/db/findAllPlace");
-  }
+	createPlace: (place) => {
+		return axios.post(`/db/createPlace`, {
+			place
+		});
+	},
+	findAllPlace: () => {
+		return axios.get('/db/findAllPlace');
+	},
+	findOnePlace: (id) => {
+		return axios.get('/db/findOnePlace', { id });
+	}
 };
 
 export default db;
