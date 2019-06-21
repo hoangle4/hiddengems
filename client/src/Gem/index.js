166aaa7b69b4f4a3e1c644f2e28b5f4d62f2568a
hoangle4 <hoang.le4@pcc.edu>
2019-06-21 11:57:56 -0700

    fix gem one

--- client/src/Gem/index.js
+++ client/src/Gem/index.js
@@ -1,47 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from "react";
 
-/* import API from "../..; */
-import Banner from './parts/Banner';
-import Story from './parts/Story';
-import SideDrawer from '../Components/SideDrawer/SideDrawer';
+import Banner from "./parts/Banner";
+import Story from "./parts/Story";
+import SideDrawer from "../Components/SideDrawer/SideDrawer";
 
-import Toolbar from '../Components/Toolbar';
-import db from '../API/placeDB';
-import './style.css';
+import Spinner from "../Components/Spinner";
+import { Consumer } from "../context";
+
+import Toolbar from "../Components/Toolbar";
+import placeDB from "../API/placeDB";
+import "./style.css";
 
 class Gem extends Component {
-	state = {
-		sideDrawerOpen: false,
-		isLoggedIn: true,
+  state = {
+    sideDrawerOpen: false,
+    isLoggedIn: true,
 
-    	data: {},
-	};
+    data: {}
+  };
 
-	componentDidMount = async () => {
-		const result = await db.findOnePlace(this.props.match.params.id);
-		this.setState({data: result.data})
-	};
+  getGem = async () => {
+    const result = await placeDB.findOnePlace(this.props.match.params.id);
+    this.setState({ data: result.data });
+  };
 
-	drawerToggleClickHandler = () => {
-		this.setState({ sideDrawerOpen: !this.state.sideDrawerOpen });
-	};
+  drawerToggleClickHandler = () => {
+    this.setState({ sideDrawerOpen: !this.state.sideDrawerOpen });
+  };
 
-	render() {
-    console.log(this.state.data)
-		return (
-			<div>
-				<Toolbar drawerClick={this.drawerToggleClickHandler} />
-				{this.state.sideDrawerOpen ? <SideDrawer isLoggedIn={this.state.isLoggedIn} /> : null}
-				<Banner image={this.state.data.photos} />
-				<div className="container">
-          <Story 
-            title={this.state.data.placeName} 
-            story={this.state.data.description}
-            />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { isAuthenticated, loading } = value;
+          return (
+            <Fragment>
+              {loading && this.state.data !== {} ? (
+                <div
+                  onLoad={() => {
+                    this.getGem();
+                  }}
+                >
+                  <Spinner />
+                </div>
+              ) : isAuthenticated ? (
+                <div>
+                  <Toolbar drawerClick={this.drawerToggleClickHandler} />
+                  {this.state.sideDrawerOpen ? (
+                    <SideDrawer isLoggedIn={this.state.isLoggedIn} />
+                  ) : null}
+                  <Banner image={this.state.data.photos} />
+                  <div className="container">
+                    <Story
+                      title={this.state.data.placeName}
+                      story={this.state.data.description}
+                    />
+                  </div>
+                </div>
+              ) : (
+                <Spinner />
+              )}
+            </Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
 }
 
 export default Gem;
