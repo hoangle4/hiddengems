e98db7bc0911e8cd94ad8de43b54e71250d23e03
Hoang Le <hoang.le4@pcc.edu>
2019-06-20 18:21:16 -0700

    user false

--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -1,78 +1,82 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import "./Login.css";
-import { Consumer } from "../context";
-import API from "../API/userDB";
+import React, { Component, Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
+import './Login.css';
+import { Consumer } from '../context';
+import API from '../API/userDB';
+import Spinner from '../Components/Spinner';
 
 class Login extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
+	state = {
+		email: '',
+		password: ''
+	};
 
-  handleOnChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
+	handleOnChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
-  loginUser = async (event, dispatch) => {
-    event.preventDefault();
-    const userInfo = {
-      email: this.state.email,
-      password: this.state.password
-    };
-    const results = await API.login(userInfo).catch(err => console.error(err));
-    if (!results) return dispatch({ type: "LOGIN_FAIL", payload: null });
-    await dispatch({
-      type: "LOGIN_SUCCESS",
-      payload: results.data.token
-    });
+	loginUser = async (event, dispatch) => {
+		event.preventDefault();
+		const userInfo = {
+			email: this.state.email,
+			password: this.state.password
+		};
+		const results = await API.login(userInfo).catch((err) => console.error(err));
+		if (!results) return dispatch({ type: 'LOGIN_FAIL', payload: null });
+		await dispatch({
+			type: 'LOGIN_SUCCESS',
+			payload: results.data.token
+		});
+	};
 
-    return <Redirect to="/dashboard" />;
-  };
-
-  render() {
-    const { email, password } = this.state;
-    return (
-      <Consumer>
-        {value => {
-          const { dispatch } = value;
-          return (
-            <div className="Signup">
-              <div className="Signup-container">
-                <form
-                  className="Signup-form"
-                  onSubmit={e => this.loginUser(e, dispatch)}
-                >
-                  <p className="Signup-input-label">Email</p>
-                  <input
-                    value={email}
-                    onChange={this.handleOnChange}
-                    className="Signup-form-field"
-                    type="email"
-                    name="email"
-                  />
-                  <p className="Signup-input-label">Password</p>
-                  <input
-                    className="Signup-form-field"
-                    type="password"
-                    name="password"
-                    value={password}
-                    onChange={this.handleOnChange}
-                  />
-                  <button className="Signup-submit-btn" type="submit">
-                    Login
-                  </button>
-                </form>
-              </div>
-            </div>
-          );
-        }}
-      </Consumer>
-    );
-  }
+	render() {
+		const { email, password } = this.state;
+		return (
+			<Consumer>
+				{(value) => {
+					const { dispatch, isAuthenticated, loading } = value;
+					return (
+						<Fragment>
+							{isAuthenticated ? (
+								<Redirect to="/dashboard" />
+							) : loading ? (
+								<Spinner />
+							) : (
+								<div className="Signup">
+									<div className="Signup-container">
+										<form className="Signup-form" onSubmit={(e) => this.loginUser(e, dispatch)}>
+											<p className="Signup-input-label">Email</p>
+											<input
+												value={email}
+												onChange={this.handleOnChange}
+												className="Signup-form-field"
+												type="email"
+												name="email"
+											/>
+											<p className="Signup-input-label">Password</p>
+											<input
+												className="Signup-form-field"
+												type="password"
+												name="password"
+												value={password}
+												onChange={this.handleOnChange}
+											/>
+											<button className="Signup-submit-btn" type="submit">
+												Login
+											</button>
+										</form>
+									</div>
+								</div>
+							)}
+						</Fragment>
+					);
+				}}
+			</Consumer>
+		);
+	}
 }
 
 export default Login;
--- client/src/Signup/index.js
+++ client/src/Signup/index.js
@@ -1,112 +1,118 @@
-import React, { Component } from "react";
-import "./Signup.css";
-import { Consumer } from "../context";
-import API from "../API/userDB";
+import React, { Component, Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
+import './Signup.css';
+import { Consumer } from '../context';
+import API from '../API/userDB';
+import Spinner from '../Components/Spinner';
 
 class Signup extends Component {
-  state = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    verify: ""
-  };
+	state = {
+		firstName: '',
+		lastName: '',
+		email: '',
+		password: '',
+		verify: ''
+	};
 
-  handleOnChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value
-    });
-  };
-  createUser = async (event, dispatch) => {
-    event.preventDefault();
-    const newuser = {
-      email: this.state.email,
-      password: this.state.password,
-      firstName: this.state.firstName,
-      lastName: this.state.lastName
-    };
+	handleOnChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			[name]: value
+		});
+	};
+	createUser = async (event, dispatch) => {
+		event.preventDefault();
+		const newuser = {
+			email: this.state.email,
+			password: this.state.password,
+			firstName: this.state.firstName,
+			lastName: this.state.lastName
+		};
 
-    const results = await API.createUser(newuser).catch(err => {
-      console.error(err);
-    });
+		const results = await API.createUser(newuser).catch((err) => {
+			console.error(err);
+		});
 
-    if (!results) return dispatch({ type: "REGISTER_FAIL", payload: null });
+		if (!results) return dispatch({ type: 'REGISTER_FAIL', payload: null });
 
-    dispatch({
-      type: "REGISTER_SUCCESS",
-      payload: results.data.token
-    });
-    //Redirect page
-  };
+		dispatch({
+			type: 'REGISTER_SUCCESS',
+			payload: results.data.token
+		});
+		//Redirect page
+	};
 
-  render() {
-    const { firstName, lastName, email, password, verify } = this.state;
-    return (
-      <Consumer>
-        {value => {
-          const { dispatch } = value;
-
-          return (
-            <div className="Signup">
-              <div className="Signup-container">
-                <form
-                  className="Signup-form"
-                  onSubmit={e => this.createUser(e, dispatch)}
-                >
-                  <p className="Signup-input-label">First Name</p>
-                  <input
-                    value={firstName}
-                    onChange={this.handleOnChange}
-                    className="Signup-form-field"
-                    type="text"
-                    name="firstName"
-                  />
-                  <p className="Signup-input-label">Last Name</p>
-                  <input
-                    value={lastName}
-                    onChange={this.handleOnChange}
-                    className="Signup-form-field"
-                    type="text"
-                    name="lastName"
-                  />
-                  <p className="Signup-input-label">Email</p>
-                  <input
-                    value={email}
-                    onChange={this.handleOnChange}
-                    className="Signup-form-field"
-                    type="email"
-                    name="email"
-                  />
-                  <p className="Signup-input-label">Password</p>
-                  <input
-                    value={password}
-                    onChange={this.handleOnChange}
-                    className="Signup-form-field"
-                    type="password"
-                    name="password"
-                  />
-                  <p className="Signup-input-label">Verify Password</p>
-                  <input
-                    value={verify}
-                    onChange={this.handleOnChange}
-                    className="Signup-form-field"
-                    type="password"
-                    name="verify"
-                  />
-                  <div className="Signup-submit-btn-container">
-                    <button className="Signup-submit-btn" type="submit">
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          );
-        }}
-      </Consumer>
-    );
-  }
+	render() {
+		const { firstName, lastName, email, password, verify } = this.state;
+		return (
+			<Consumer>
+				{(value) => {
+					const { dispatch, isAuthenticated, loading } = value;
+					return (
+						<Fragment>
+							{isAuthenticated ? (
+								<Redirect to="dashboard" />
+							) : loading ? (
+								<Spinner />
+							) : (
+								<div className="Signup">
+									<div className="Signup-container">
+										<form className="Signup-form" onSubmit={(e) => this.createUser(e, dispatch)}>
+											<p className="Signup-input-label">First Name</p>
+											<input
+												value={firstName}
+												onChange={this.handleOnChange}
+												className="Signup-form-field"
+												type="text"
+												name="firstName"
+											/>
+											<p className="Signup-input-label">Last Name</p>
+											<input
+												value={lastName}
+												onChange={this.handleOnChange}
+												className="Signup-form-field"
+												type="text"
+												name="lastName"
+											/>
+											<p className="Signup-input-label">Email</p>
+											<input
+												value={email}
+												onChange={this.handleOnChange}
+												className="Signup-form-field"
+												type="email"
+												name="email"
+											/>
+											<p className="Signup-input-label">Password</p>
+											<input
+												value={password}
+												onChange={this.handleOnChange}
+												className="Signup-form-field"
+												type="password"
+												name="password"
+											/>
+											<p className="Signup-input-label">Verify Password</p>
+											<input
+												value={verify}
+												onChange={this.handleOnChange}
+												className="Signup-form-field"
+												type="password"
+												name="verify"
+											/>
+											<div className="Signup-submit-btn-container">
+												<button className="Signup-submit-btn" type="submit">
+													Submit
+												</button>
+											</div>
+										</form>
+									</div>
+								</div>
+							)}
+						</Fragment>
+					);
+				}}
+			</Consumer>
+		);
+	}
 }
 
 export default Signup;
