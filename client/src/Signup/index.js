676164146fc70cad8b0ecc8347d10d338d7155e0
Ryan Grunest <ryangrunest45@gmail.com>
2019-06-22 12:09:31 -0700

    form updates with nested state vals

--- client/src/Signup/index.js
+++ client/src/Signup/index.js
@@ -7,17 +7,32 @@ import Spinner from '../Components/Spinner';
 
 class Signup extends Component {
 	state = {
-		firstName: '',
-		lastName: '',
-		email: '',
-		password: '',
-		verify: '',
-		errorMsg: ''
+		firstName: {
+			text: '',
+			errMsg: '',
+		},
+		lastName: {
+			text: '',
+			errMsg: '',
+		},
+		email: {
+			text: '',
+			errMsg: '',
+		},
+		password: {
+			text: '',
+			errMsg: '',
+		},
+		verify: {
+			text: '',
+			errMsg: '',
+		},
+		errCount: 0,
 	};
 
-	checkIfEmpty = (value, name) => {
+	checkIfEmpty = (target, name) => {
 		let stringVal = '';
-		if (value === "") {
+		if (target.text === "") {
 			stringVal = `${name} must not be empty.`
 		}
 		return stringVal;
@@ -25,7 +40,7 @@ class Signup extends Component {
 
 	checkEmail = () => {
 		let stringVal = '';
-		let e = this.state.email;
+		let e = this.state.email.text;
 		if (e.includes('@') === false || (e.includes('.com') === false && e.includes('.net') === false && e.includes('.org') === false)) {
 			stringVal += "Email must contain correct formatting, i.e info@hiddengems.com";
 		} else {
@@ -37,27 +52,38 @@ class Signup extends Component {
 	handleOnChange = (e) => {
 		const { name, value } = e.target;
 		this.setState({
-			[name]: value
+			[name]: { text: value, errMsg: this.state[name].errMsg }
 		});
 	};
 
+	handleErrors = (target, name) => {
+		let errText = '';
+		errText += this.checkIfEmpty(target, name);
+		if (errText !== '') {
+			this.setState({
+				[target.errMsg] : errText,
+			});
+			this.setState((prevState, props) => ({
+				errCount: prevState.errCount + 1
+			}));
+		}
+	}
+
 	createUser = async (event, dispatch) => {
 		event.preventDefault();
-		let errText = '';
 
-		errText += ' ' + this.checkIfEmpty(this.state.email, 'Email input');
-		errText += ' ' + this.checkIfEmpty(this.state.password, 'Password input');
-		errText += ' ' + this.checkIfEmpty(this.state.firstName, 'First name input');
-		errText += ' ' + this.checkIfEmpty(this.state.lastName, 'Last name input');
-		
-		errText += ' ' + this.checkEmail();
+		this.handleErrors(this.state.email, 'Email');
+		this.handleErrors(this.state.password, 'Password');
+		this.handleErrors(this.state.firstName, 'First name');
+		this.handleErrors(this.state.lastName, 'Last name');
 
-		if (errText === '') {
+
+		if (this.state.errCount === 0) {
 			const newuser = {
-				email: this.state.email,
-				password: this.state.password,
-				firstName: this.state.firstName,
-				lastName: this.state.lastName
+				email: this.state.email.text,
+				password: this.state.password.text,
+				firstName: this.state.firstName.text,
+				lastName: this.state.lastName.text
 			};
 
 			const results = await API.createUser(newuser).catch((err) => {
@@ -73,10 +99,10 @@ class Signup extends Component {
 
 			//Redirect page
 		} else {
-			this.setState({
-				errorMsg: errText,
+			// this.setState({
+			// 	errorMsg: errText,
 				
-			}, () => {alert(this.state.errorMsg)})
+			// }, () => {alert(this.state.errorMsg)})
 			// alert(this.state.errorMsg);
 		}
 	};
@@ -99,49 +125,54 @@ class Signup extends Component {
 										<form className="Signup-form" onSubmit={(e) => this.createUser(e, dispatch)} noValidate>
 											<p className="Signup-input-label">First Name</p>
 											<input
-												value={firstName}
+												value={firstName.text}
 												onChange={this.handleOnChange}
 												className="Signup-form-field"
 												type="text"
 												name="firstName"
 												required
 											/>
+											<p><small>{firstName.errMsg}</small></p>
 											<p className="Signup-input-label">Last Name</p>
 											<input
-												value={lastName}
+												value={lastName.text}
 												onChange={this.handleOnChange}
 												className="Signup-form-field"
 												type="text"
 												name="lastName"
 												required
 											/>
+											<p><small>{lastName.errMsg}</small></p>
 											<p className="Signup-input-label">Email</p>
 											<input
-												value={email}
+												value={email.text}
 												onChange={this.handleOnChange}
 												className="Signup-form-field"
 												type="email"
 												name="email"
 												required
 											/>
+											<p><small>{email.errMsg}</small></p>
 											<p className="Signup-input-label">Password</p>
 											<input
-												value={password}
+												value={password.text}
 												onChange={this.handleOnChange}
 												className="Signup-form-field"
 												type="password"
 												name="password"
 												required
 											/>
+											<p><small>{password.errMsg}</small></p>
 											<p className="Signup-input-label">Verify Password</p>
 											<input
-												value={verify}
+												value={verify.text}
 												onChange={this.handleOnChange}
 												className="Signup-form-field"
 												type="password"
 												name="verify"
 												required
 											/>
+											<p><small>{verify.errMsg}</small></p>
 											<div className="Signup-submit-btn-container">
 												<button className="Signup-submit-btn" type="submit">
 													Submit
