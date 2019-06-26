3cb707aa17b8f55e79de1ca9b2527bf4e69ca8f7
Hoang Le <hoang.le4@pcc.edu>
2019-06-25 19:20:29 -0700

    commit CommentList

--- client/src/Gem/parts/CommentList.js
+++ client/src/Gem/parts/CommentList.js
@@ -1,10 +1,15 @@
-import React from "react";
-
-const CommentList = () => {
-  return (
-    <div className="CommentList_box">
-      <h1 className="Comment_h1"> Comment </h1>
-    </div>
-  );
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+const CommentList = ({ comments: { commentMessage, commentTitle, commentUser } }) => {
+	useEffect = () => {};
+	return (
+		<div className="CommentList_commentbox">
+			<Link to={`/profile/${commentUser}`}>
+				<img className="Comment_list_img" src="..." alt="..." />
+			</Link>
+			<h5 className="CommentList_h5">{commentTitle}</h5>
+			<p>{commentMessage}</p>
+		</div>
+	);
 };
 export default CommentList;
