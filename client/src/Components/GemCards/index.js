2d24263563e341419f9fc625e82e8b183bc46127
Ryan Grunest <ryangrunest45@gmail.com>
2019-06-27 10:44:56 -0700

    gemcard styling

--- client/src/Components/GemCards/index.js
+++ client/src/Components/GemCards/index.js
@@ -5,23 +5,28 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function GemCards({ placeCreated }) {
   return (
-    <div className="gemContainer">
+    <div className="GemCards-gemContainer">
       {placeCreated.map(place => (
-        <a key={place._id} href={`/gem/${place._id}`} className="gemLink">
-          <div className="gemBox">
+        <a key={place._id} href={`/gem/${place._id}`} className="GemCards-gemLink">
+          <div className="GemCards-gemBox">
             <img
+              className="GemCards-Image"
               style={{ width: "200px", height: "200px" }}
               src={place.photos}
               alt={place.placeName}
             />
-            <h3>{place.placeName}</h3>
-            <p className="truncGem">{place.description}</p>
-            <button type="submit" className="editBtn" id={place._id}>
-              Edit
-            </button>
-            <button type="submit" className="deleteBtn" id={place._id}>
-              Delete
-            </button>
+            <div className="GemCards-text-container">
+              <h3 className="GemCards-Name">{place.placeName}</h3>
+              <p className="GemCards-truncGem">{place.description}</p>
+            </div>
+            <div className="GemCards-button-container">
+              <button type="submit" className="GemCards-editBtn" id={place._id}>
+                <i className="fas fa-edit"></i>
+              </button>
+              <button type="submit" className="GemCards-deleteBtn" id={place._id}>
+                <i className="fas fa-trash"></i>
+              </button>
+            </div>
           </div>
         </a>
       ))}
