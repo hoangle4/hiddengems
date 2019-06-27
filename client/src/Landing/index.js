1d25eee4e010f94be958913b1935610ca7ae6bac
hoangle4 <hoang.le4@pcc.edu>
2019-06-26 21:27:01 -0700

    commit fetching

--- client/src/Landing/index.js
+++ client/src/Landing/index.js
@@ -1,57 +1,65 @@
-import React from 'react';
-import './Landing.css';
-import SearchForm from './SearchForm';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Landing.css";
+import SearchForm from "./SearchForm";
 
 const LandingPage = () => {
-	return (
-		<div>
-			<div className="Landing">
-				<div className="Landing-header">
-					<div className="Landing-title">
-						<div>
-							<p>H</p>
-							<p>I</p>
-							<p>D</p>
-						</div>
-						<div>
-							D <i id="Landing-gem" className="fas fa-gem" />N
-						</div>
-						<div>
-							<p>G</p>
-							<p>E</p>
-							<p>M</p>
-						</div>
-					</div>
+  return (
+    <div>
+      <div className="Landing">
+        <div className="Landing-header">
+          <div className="Landing-title">
+            <div>
+              <p>H</p>
+              <p>I</p>
+              <p>D</p>
+            </div>
+            <div>
+              D <i id="Landing-gem" className="fas fa-gem" />N
+            </div>
+            <div>
+              <p>G</p>
+              <p>E</p>
+              <p>M</p>
+            </div>
+          </div>
 
-					<ul className="Landing-nav">
-						<li className="Landing-nav-links">
-							<a href="login">Login</a>
-						</li>
-						<li className="Landing-nav-links">
-							<a href="signup">Sign Up</a>
-						</li>
-					</ul>
-				</div>
-				<img className="Landing-mockup" src={'./mockup.png'} alt="iphone mockup" />
-				<div className="Landing-form-container">
-					<SearchForm />
-				</div>
-			</div>
-			<div className="Landing-bottom">
-				<ul className="Landing-bottom-list">
-					<li className="Landing-bottom-bullets">
-						<i className="fas fa-search" />Discover new places.
-					</li>
-					<li className="Landing-bottom-bullets">
-						<i className="fas fa-book-open" />Create new stories.
-					</li>
-					<li className="Landing-bottom-bullets">
-						<i className="fas fa-user-friends" />Share with your friends.
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+          <ul className="Landing-nav">
+            <li className="Landing-nav-links">
+              <Link to="login">Login</Link>
+            </li>
+            <li className="Landing-nav-links">
+              <Link to="signup">Sign Up</Link>
+            </li>
+          </ul>
+        </div>
+        <img
+          className="Landing-mockup"
+          src={"./mockup.png"}
+          alt="iphone mockup"
+        />
+        <div className="Landing-form-container">
+          <SearchForm />
+        </div>
+      </div>
+      <div className="Landing-bottom">
+        <ul className="Landing-bottom-list">
+          <li className="Landing-bottom-bullets">
+            <i className="fas fa-search" />
+            Discover new places.
+          </li>
+          <li className="Landing-bottom-bullets">
+            <i className="fas fa-book-open" />
+            Create new stories.
+          </li>
+          <li className="Landing-bottom-bullets">
+            <i className="fas fa-user-friends" />
+            Share with your friends.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
