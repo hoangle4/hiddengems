2d02f2806a649b820389881cb99d8232b2136f8a
hoangle4 <hoang.le4@pcc.edu>
2019-06-29 20:07:38 -0700

    commit SideNav bg

--- client/src/Components/MapGem/index.js
+++ client/src/Components/MapGem/index.js
@@ -1,52 +1,52 @@
-import React, { Fragment, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Truncate } from '../../Helper';
-import Spinner from '../Spinner';
-import './style.css';
-function SideGem({ data: { photos, placeName, description, _id }, handleCloseSideBar, isMarkerClicked }) {
-	const [ truncatedTitle, setTruncatedTitle ] = useState('');
-	console.log(truncatedTitle);
-	// const initTruncate = () => {
-	// 	if (isMarkerClicked) ;
-	// };
-	useEffect(
-		() => {
-			if (!placeName) return;
-			setTruncatedTitle(Truncate(placeName, 25));
-		},
-		[ placeName ]
-	);
-	return (
-		<Fragment>
-			<ul className="MainMap_Sidenav">
-				<li>
-					<b>
-						<Link className="MapGem_Title" to={`/gem/${_id}`}>
-							{truncatedTitle}
-						</Link>
-					</b>
-				</li>
-				<li>
-					<img src={photos} className="MapGem_Img" alt="..." />
-				</li>
+import React, { Fragment, useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Truncate } from "../../Helper";
+import Spinner from "../Spinner";
+import "./style.css";
+function SideGem({
+  data: { photos, placeName, description, _id },
+  handleCloseSideBar
+}) {
+  const [truncatedTitle, setTruncatedTitle] = useState("");
+  const [truncatedDescription, setTruncatedDescription] = useState("");
 
-				<li className="MapGem_Description">
-					<b>{description}</b>
-				</li>
-				<li>
-					<Link to={`/gem/${_id}`}>
-						<i className="fa fa-book-reader" />
-						<b>Read More</b>
-					</Link>
-				</li>
-				<li>
-					<a href="#" onClick={handleCloseSideBar}>
-						<i className="fa fa-times" />
-						<b>Close</b>
-					</a>
-				</li>
-			</ul>
-		</Fragment>
-	);
+  useEffect(() => {
+    if (!placeName) return;
+    setTruncatedTitle(Truncate(placeName, 30));
+    if (!description) return;
+    setTruncatedDescription(Truncate(description, 300));
+  }, [placeName, description]);
+  return (
+    <Fragment>
+      <ul className="MainMap_Sidenav">
+        <li className="MapGem_Title_Li">
+          <b>
+            <Link className="MapGem_Title" to={`/gem/${_id}`}>
+              {truncatedTitle}
+            </Link>
+          </b>
+        </li>
+        <li className="MapGem_Crop_Img">
+          <img src={photos} className="MapGem_Img" alt="..." />
+        </li>
+
+        <li className="MapGem_Description">
+          <b>{truncatedDescription}</b>
+        </li>
+        <li className="MapGem_Button_Li">
+          <Link to={`/gem/${_id}`}>
+            <i className="fa fa-book-reader" />
+            <b>Read More</b>
+          </Link>
+        </li>
+        <li className="MapGem_Button_Li">
+          <a href="#" onClick={handleCloseSideBar}>
+            <i className="fa fa-times" />
+            <b>Close</b>
+          </a>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 export default SideGem;
