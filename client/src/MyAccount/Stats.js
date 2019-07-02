4b4527f5635ac41679971b264c5c3047f096f8d4
hoangle4 <hoang.le4@pcc.edu>
2019-07-02 03:27:34 -0700

    commit like count

--- client/src/MyAccount/Stats.js
+++ client/src/MyAccount/Stats.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from "react";
 import "./stats.css";
 const Statistics = ({ user: { follower, following, placeCreated }, user }) => {
   const [comments, setComments] = useState(0);
+  const [likes, setLikes] = useState(0);
   let commentCount = 0;
-  const countComments = () => {
-    placeCreated.forEach(place => (commentCount += place.comments.length));
+  let likeCount = 0;
+  const count = () => {
+    placeCreated.forEach(place => {
+      commentCount += place.comments.length;
+      likeCount += place.likes.length;
+    });
     setComments(commentCount);
+    setLikes(likeCount);
   };
-  useEffect(() => countComments(), []);
-
+  useEffect(() => count(), []);
+  console.log(placeCreated);
   return (
     <div className="Stats">
       <div className="Stats_Main">
@@ -38,7 +44,7 @@ const Statistics = ({ user: { follower, following, placeCreated }, user }) => {
         <div className="Stats_Right">
           <div className="Stats_Likes">
             <i className="fas fa-thumbs-up  Stats_Icon" />
-            <h2 className=" Stats_Count_Title ">6</h2>
+            <h2 className=" Stats_Count_Title ">{likes}</h2>
             <div className="Stats_Line_Black" />
             <p className="Stats_Text">Likes</p>
           </div>
