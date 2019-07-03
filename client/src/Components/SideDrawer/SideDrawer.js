968b3ec095135d91056292514d3fb68639860cf3
Hoang Le <hoang.le4@pcc.edu>
2019-07-02 19:01:58 -0700

    commit files

--- client/src/Components/SideDrawer/SideDrawer.js
+++ client/src/Components/SideDrawer/SideDrawer.js
@@ -1,63 +1,64 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import "./SideDrawer.css";
-import { Consumer } from "../../context";
-import Spinner from "../Spinner";
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import './SideDrawer.css';
+import { Consumer } from '../../context';
+import Spinner from '../Spinner';
 
 function SideDrawer() {
-  return (
-    <Consumer>
-      {value => {
-        const { isAuthenticated, loading, dispatch } = value;
-        return (
-          <Fragment>
-            {loading ? (
-              <Spinner />
-            ) : isAuthenticated ? (
-              <ul className="SideDrawer_Sidenav">
-                <li className="MapGem_Button_Li">
-                  <Link className="MapGem_Button_Container" to="/">
-                    <i className="fa fa-home MapGem_Button_Icon" />
-                    <b className="MapGem_Button_Text">Home</b>
-                  </Link>
-                </li>
-                <li className="MapGem_Button_Li">
-                  <Link className="MapGem_Button_Container" to="/dashboard">
-                    <i className="fa fa-user MapGem_Button_Icon" />
-                    <b className="MapGem_Button_Text">Dashboard</b>
-                  </Link>
-                </li>
-                <li className="MapGem_Button_Li">
-                  <Link className="MapGem_Button_Container"
-                    to="/"
-                    onClick={() => dispatch({ type: "LOG_OUT", payload: null })}
-                  >
-                    <i className="fa fa-power-off MapGem_Button_Icon" />
-                    <b className="MapGem_Button_Text">Log Out</b>
-                  </Link>
-                </li>
-              </ul>
-            ) : (
-              <ul className="SideDrawer_Sidenav">
-                <li className="MapGem_Button_Li">
-                  <Link to="/login">
-                    <i className="fa fa-arrow-right" />
-                    <b>Login</b>
-                  </Link>
-                </li>
-                <li className="MapGem_Button_Li">
-                  <Link to="/signup">
-                    <i className="fa fa-home" />
-                    <b>Home</b>
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </Fragment>
-        );
-      }}
-    </Consumer>
-  );
+	return (
+		<Consumer>
+			{(value) => {
+				const { isAuthenticated, loading, dispatch } = value;
+				return (
+					<Fragment>
+						{loading ? (
+							<Spinner />
+						) : isAuthenticated ? (
+							<ul className="SideDrawer_Sidenav">
+								<li className="MapGem_Button_Li">
+									<Link className="MapGem_Button_Container" to="/">
+										<i className="fa fa-home MapGem_Button_Icon" />
+										<b className="MapGem_Button_Text">Home</b>
+									</Link>
+								</li>
+								<li className="MapGem_Button_Li">
+									<Link className="MapGem_Button_Container" to="/dashboard">
+										<i className="fa fa-user MapGem_Button_Icon" />
+										<b className="MapGem_Button_Text">Dashboard</b>
+									</Link>
+								</li>
+								<li className="MapGem_Button_Li">
+									<Link
+										className="MapGem_Button_Container"
+										to="/"
+										onClick={() => dispatch({ type: 'LOG_OUT', payload: null })}
+									>
+										<i className="fa fa-power-off MapGem_Button_Icon" />
+										<b className="MapGem_Button_Text">Log Out</b>
+									</Link>
+								</li>
+							</ul>
+						) : (
+							<ul className="SideDrawer_Sidenav">
+								<li className="MapGem_Button_Li">
+									<Link to="/login" className="MapGem_Button_Container">
+										<i className="fa fa-arrow-right MapGem_Button_Icon" />
+										<b className="MapGem_Button_Text">Login</b>
+									</Link>
+								</li>
+								<li className="MapGem_Button_Li">
+									<Link to="/signup" className="MapGem_Button_Container">
+										<i className="fa fa-home MapGem_Button_Icon" />
+										<b className="MapGem_Button_Text">Home</b>
+									</Link>
+								</li>
+							</ul>
+						)}
+					</Fragment>
+				);
+			}}
+		</Consumer>
+	);
 }
 
 export default SideDrawer;
